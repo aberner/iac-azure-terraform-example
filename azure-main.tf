4b1e2a34758e5ac1f98613c018d6878ec9b72dd4
Arne Berner <arne.berner@bouvet.no>
2020-11-15 19:10:31 +0100

    adding a sqlserver

--- azure-main.tf
+++ azure-main.tf
@@ -138,6 +138,37 @@ resource "azurerm_linux_virtual_machine" "azure-web-vm" {
   }
 }
 
+resource "azurerm_sql_server" "azure-sqlserver" {
+  name                         = "${var.app_name}-${var.app_environment}-sqlserver"
+  resource_group_name          = azurerm_resource_group.azure-rg.name
+  location                     = azurerm_resource_group.azure-rg.location
+  version                      = "12.0"
+  administrator_login          = var.linux_admin_user
+  administrator_login_password = var.linux_admin_password
+
+  tags = {
+    environment = var.app_environment,
+    responsible = var.department_id
+  }
+}
+
+resource "azurerm_mssql_database" "test" {
+  name           = "${var.app_name}-${var.app_environment}-sqldb"
+  server_id      = azurerm_sql_server.azure-sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "BC_Gen5_2"
+  zone_redundant = true
+
+  tags = {
+    environment = var.app_environment,
+    responsible = var.department_id
+  }
+
+}
+
 #Output
 output "external-ip-azure-web-server" {
   value = azurerm_public_ip.azure-web-ip.ip_address
