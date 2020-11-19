2e515816036238a21a598e72bd61516cbef4f25a
philip.nerem <philip.nerem@bouvet.no>
2020-11-19 17:56:49 +0100

    removed variable for admin password

--- azure-main.tf
+++ azure-main.tf
@@ -1,3 +1,13 @@
+#Configure the state location
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "philip-tfstate"
+    storage_account_name  = "philstfstate"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
 #Configure the Azure Provider
 provider "azurerm" {
   version = ">= 2.33"
@@ -118,6 +128,8 @@ resource "azurerm_linux_virtual_machine" "azure-web-vm" {
   admin_password = var.linux_admin_password
   disable_password_authentication = false
 
+
+
   source_image_reference {
     publisher = var.ubuntu-linux-publisher
     offer     = var.ubuntu-linux-offer
@@ -131,11 +143,40 @@ resource "azurerm_linux_virtual_machine" "azure-web-vm" {
     storage_account_type = "Standard_LRS"
   }
 
+  # It's easy to transfer files or templates using Terraform.
+  provisioner "file" {
+    source      = "files/setup.sh"
+    destination = "/home/${var.linux_admin_user}/setup.sh"
+
+    connection {
+      type     = "ssh"
+      user     = var.linux_admin_user
+      password = var.linux_admin_password
+      host     = azurerm_public_ip.azure-web-ip.ip_address
+    }
+  }
+
+  # This shell script starts our Apache server and prepares the demo environment.
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/${var.linux_admin_user}/setup.sh",
+      "sudo /home/${var.linux_admin_user}/setup.sh",
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = var.linux_admin_user
+      password = var.linux_admin_password
+      host     = azurerm_public_ip.azure-web-ip.ip_address
+    }
+  }
+
   tags = {
     environment = var.app_environment,
     responsible = var.department_id
   }
 }
+
 
 #Output
 output "external-ip-azure-web-server" {
