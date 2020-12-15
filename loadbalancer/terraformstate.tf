356fbcdf9aba7be6874f46c5647a7028e0cc1010
philipnadm <73950930+philipnadm@users.noreply.github.com>
2020-12-15 14:41:16 +0100

    backend again

--- loadbalancer/terraformstate.tf
+++ loadbalancer/terraformstate.tf
@@ -1,9 +1,9 @@
 #Configure the backend
 terraform {
   backend "azurerm" {
-    resource_group_name  = "philip-tfstate"
-    storage_account_name = "philstfstate"
-    container_name       = "tfstate"
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
     key                  = "terraform.tfstate"
   }
 }
